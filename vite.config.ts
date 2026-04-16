58d793dfb0f6def9ce1a5b5bb52bcc3a116e044e
ABC <aabidshora@gmail.com>
2026-04-16 19:17:40 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,12 +11,16 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), tailwindcss()],
-  base: '/',
+  plugins: [react(), tailwindcss(),viteSingleFile()],
+  base: mode === 'production' ? '/GRB-TRAVEL/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
   server: {
     port: 3000,
